2aaa93805797dfe9940bf891bc0e3b470ccca13f
Gustavo Medeiros <eugus03@gmail.com>
2023-01-13 22:07:18 -0300

    style: picture width

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,25 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          <h2 className="text-4xl sm:text-6xl font-bold text-white">
             Desenvolvedor JavaScript.
           </h2>
-          <p className="text-gray-500 py-4">Eu sou desenvolvedor JavaScript.</p>
+          <p className="text-gray-400 py-4 max-w-md">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis,
+            fugiat!
+          </p>
           <div>
-            <button>
+            <button
+              className="group text-white font-semibold w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r
+             from-yellow-400 to-yellow-800 cursor-pointer"
+            >
               Portfólio
-              <span>
-                <MdOutlineKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={22} className="ml-1" />
               </span>
             </button>
           </div>
@@ -28,7 +34,7 @@ const Home = () => {
           <img
             src={MySelf}
             alt="Gustavo"
-            className="rounded-full mx-auto w-2/3 md:w-72"
+            className="rounded-full mx-auto w-2/3 md:w-96"
           />
         </div>
       </div>
