0cbba4954b5dba2cf020fd747010535f394269d3
Gustavo Medeiros <eugus03@gmail.com>
2023-01-16 17:45:38 -0300

    changing my about component

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -14,11 +14,14 @@ const Sobre = () => {
         </div>
 
         <p className="text-xl mt-2">
-          Olá, meu nome é Gustavo Medeiros, tenho 19 anos e sou um desenvolvedor
-          apaixonado por soluções simples para problemas difíceis. Além de
-          entusiasta de métodos ágeis, ambientes flexíveis e entrega contínua.
-          Atualmente estagio em uma software house, curso gestão da TI na
-          Fatec-SP e sou aluno de programação na Alura!
+          Olá, meu nome é Gustavo Medeiros Thomaz e sou um programador ambicioso
+          por soluções simples para problemas difíceis. Tenho experiência em
+          projetos front-end utilizando JavaScript, React e Tailwind CSS. Noções
+          em back-end com Node.js, TypeScript, MongoDB e .NET na construção de
+          APIs. Atualmente estagio em uma software house, curso gestão da TI na
+          Fatec-SP e sou aluno de programação na Alura! Minhas soft skills:
+          Trabalho em equipe, comunicação, adaptabilidade, pensamento crítico e
+          solução de problemas.
         </p>
 
         <br />
